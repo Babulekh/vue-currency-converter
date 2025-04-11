61e9c4a23784b10c4e571cdc5edf32412e663b73
Babulekh <vanca624@gmail.com>
2025-04-11 02:49:40 +0200

    Added currency select dropdown

---
+++ src/pages/MainView/Header/CurrencyDropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <VCombobox
+    :options="supportedCurrenciesOptions"
+    v-model:selected-option-id="currentSelectedCurrency" />
+</template>
+
+<script setup lang="ts">
+import type { Option } from '@/shared/ui/combobox'
+
+import { storeToRefs } from 'pinia'
+import { supportedCurrencies, useCurrencyStore } from '@/entities/currency'
+
+import VCombobox from '@/shared/ui/combobox/VCombobox.vue'
+
+const supportedCurrenciesOptions: Array<Option> = supportedCurrencies.map((currencyName) => ({
+  id: currencyName,
+  label: currencyName
+}))
+
+const { currentSelectedCurrency } = storeToRefs(useCurrencyStore())
+</script>
